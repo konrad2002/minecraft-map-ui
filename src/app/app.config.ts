d91d2e2455679fc5de0a644c2ec516dafb8ee25c
konrad2002 <konrad@schwimmteamerzgebirge.de>
2025-05-03 01:28:16 +0200

    update to angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import {routes} from "./app.routes";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {apiKeyInterceptor} from "./core/interceptor/api-key.interceptor";
